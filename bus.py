67674a81b4cf6419725b0379b6e2f7463189fa27
votinh1210 <vuminh.dinh@belcurves.com>
2017-09-24 17:24:18 +0200

    fix bugs

--- bus.py
+++ bus.py
@@ -3,6 +3,7 @@ import re
 import os
 import time
 
+
 #27 CARRAS / PROMENADE
 #9  ALBERT 1er VERDUN
 stationNumber = 9
@@ -13,13 +14,16 @@ patternTime = "(?P<time>.+?)direction <span class=\"txtbold\">(?P<direction>.+?)
 def getTime(timeSection):
     #print timeSection
     pattern = re.compile("<span class=\"txtbold\">.+?</span>")
-    textBold = re.search(pattern, timeSection).group()
-    if 'h' in textBold:
-        time = textBold[22:len(textBold)-7]    #<span class="txtbold">11h27</span>
-    elif textBold:
-        time = textBold[22:len(textBold)-7]    #<span class="txtbold">13</span>
-    else:
+    textBold = re.search(pattern, timeSection)
+    if textBold == None:
         time = '0'
+    else:
+        textBold = textBold.group()
+        if 'h' in textBold:
+            time = textBold[22:len(textBold)-7]    #<span class="txtbold">11h27</span>
+        else: 
+            time = textBold[22:len(textBold)-7]    #<span class="txtbold">13</span>
+        
     return time
     
 def getTimeNow(text):
@@ -35,6 +39,7 @@ def getAll(content):
         print "Bus " + busMatches.group('line') + " is comming"
         for timeMatches in re.finditer(patternTime, busMatches.group('timesheet'), re.IGNORECASE | re.DOTALL):    #for each line in timesheet
             busTime = getTime(timeMatches.group('time'))
+            timeMatches.group('direction')
             print (("\tat " if 'h' in busTime else "\tin ") + busTime + " min direction " + 
                   timeMatches.group('direction') +
                   timeMatches.group('isRealtime').strip())
@@ -48,8 +53,8 @@ def main():
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
         getInfoByStation(stationNumber)
-        #time.sleep(5)
-        break
+        time.sleep(20)
+        #break
         
 if __name__ == "__main__": 
     main()
