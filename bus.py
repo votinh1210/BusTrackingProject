c0841963491eff85114f38ecae877b3e62933f78
votinh1210 <vuminh.dinh@belcurves.com>
2017-09-24 14:03:23 +0200

    version complet

--- bus.py
+++ bus.py
@@ -6,24 +6,50 @@ import time
 #27 CARRAS / PROMENADE
 #9  ALBERT 1er VERDUN
 stationNumber = 9
-pattern = "<div class=\"data\">\s*<span class=\"txtbold\">Ligne</span> : (?P<line>.+?)\s*<div>(?P<article>.+?) <span class=\"txtbold\">(?P<time>.*?)</span>.+?direction <span class=\"txtbold\">(?P<destination>.*?)</span>"
+patternBus = "<div class=\"data\">\s*?<span class=\"txtbold\">Ligne</span> : (?P<line>.+?)\s*?<div>(?P<timesheet>.*?)\s*?</div>\s*?</div>"
+patternTime = "(?P<time>.+?)direction <span class=\"txtbold\">(?P<direction>.+?)</span>(?P<isRealtime>.*?)<br />"
 
-def getTime():
-    content = urllib.urlopen("https://cg06.tsi.cityway.fr/qrcode/" + str(stationNumber)).read()
-    #content = url.read().decode('utf-8')
-    #print (content)
-    os.system('cls' if os.name == 'nt' else 'clear')
-    for matches in re.finditer(pattern, content, re.IGNORECASE | re.DOTALL):
-        print("Bus " + matches.group('line') + " destination " + 
-                       matches.group('destination') + " is comming " + 
-                       ('in ' if matches.group('article') == 'dans' else 'at ') + 
-                       matches.group('time'))
-    time.sleep(5)
 
+def getTime(timeSection):
+    #print timeSection
+    pattern = re.compile("<span class=\"txtbold\">.+?</span>")
+    textBold = re.search(pattern, timeSection).group()
+    if 'h' in textBold:
+        time = textBold[22:len(textBold)-7]    #<span class="txtbold">11h27</span>
+    elif textBold:
+        time = textBold[22:len(textBold)-7]    #<span class="txtbold">13</span>
+    else:
+        time = '0'
+    return time
+    
+def getTimeNow(text):
+    patternTimeNow = re.compile("<span>Il est</span>\s*?<span class=\"txtbold\">.+?</span>")
+    timeNowSection = re.search(patternTimeNow, text).group()
+    timeNow = re.search(r'\d\d?h\d\d', timeNowSection).group()
+    return timeNow
+
+def getAll(content):
+    print "Now is " + getTimeNow(content) + "\n"
+    
+    for busMatches in re.finditer(patternBus, content, re.IGNORECASE | re.DOTALL):    #for each line of bus
+        print "Bus " + busMatches.group('line') + " is comming"
+        for timeMatches in re.finditer(patternTime, busMatches.group('timesheet'), re.IGNORECASE | re.DOTALL):    #for each line in timesheet
+            busTime = getTime(timeMatches.group('time'))
+            print (("\tat " if 'h' in busTime else "\tin ") + busTime + " min direction " + 
+                  timeMatches.group('direction') +
+                  timeMatches.group('isRealtime').strip())
+                  
+def getInfoByStation(station):
+    content = urllib.urlopen("https://cg06.tsi.cityway.fr/qrcode/" + str(station)).read()
+    #print content
+    getAll(content)
+    
 def main():
     while True:
-        getTime()
-        
+        os.system('cls' if os.name == 'nt' else 'clear')
+        getInfoByStation(stationNumber)
+        #time.sleep(5)
+        break
         
 if __name__ == "__main__": 
     main()
