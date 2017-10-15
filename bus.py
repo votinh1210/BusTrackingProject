dd3501cf58448dbeec47fc59714d79857df77427
votinh1210 <vuminh.dinh@belcurves.com>
2017-10-16 00:13:46 +0200

    big update

--- bus.py
+++ bus.py
@@ -1,44 +1,54 @@
 #!/usr/bin/python
-
+from __future__ import print_function
 import urllib
 import re
 import os
 import time
+import datetime
+from collections import OrderedDict
 
-
-#######      Sophia - Nice      #######
 #9      ALBERT 1er VERDUN
-#2064      LES TEMPLIERS
-#2155     SKEMA
 
+mappingStationSophiaNice = OrderedDict()
+mappingStationSophiaNice['2155']=   'SKEMA    '
+mappingStationSophiaNice['2064']=   'TEMPLIERS'
 
-#######      Nice - Sophia      #######
-#81     LYCEE MASSENA
-#43     GUSTAVE V
-#32     GAMBETTA / PROMENADE
-#84     MAGNAN / PROMENADE
-#25     FABRON MUSEE D'ART NAIF
-#27     CARRAS / PROMENADE
-#968    FERBER / PROMENADE
-#4      AEROPORT / PROMENADE
-#2148   SANTOLINE
+mappingStationNiceSophia = OrderedDict()
+mappingStationNiceSophia['81']=   'MASSENA  '
+mappingStationNiceSophia['43']=   'GUSTAVE  '
+mappingStationNiceSophia['32']=   'GAMBETTA '
+mappingStationNiceSophia['84']=   'MAGNAN   '
+mappingStationNiceSophia['25']=   'FABRON   '
+mappingStationNiceSophia['27']=   'CARRAS   '
+mappingStationNiceSophia['56']=   'VALLIERE '
+mappingStationNiceSophia['4']=    'AEROPORT '
+mappingStationNiceSophia['2148']= 'SANTOLINE'
 
+timeSheetNiceSophia = OrderedDict()
+
+lineNeedToTrack = '230'
+destinationToTrack = 'Sophia'  
+timeCheck = 0
+    
+    
 patternBus = "<div class=\"data\">\s*?<span class=\"txtbold\">Ligne</span> : (?P<line>.+?)\s*?<div>(?P<timesheet>.*?)\s*?</div>\s*?</div>"
 patternTime = "(?P<time>.+?)direction <span class=\"txtbold\">(?P<direction>.+?)</span>(?P<isRealtime>.*?)<br />"
 
+def setTimeToStandardFormat(elapsedTime):
+    return (datetime.datetime.now() + datetime.timedelta(minutes = int(elapsedTime))).strftime("%Hh%M")
 
-def getTime(timeSection):
-    #print timeSection
+def getTimeFrom(timeSection):
     pattern = re.compile("<span class=\"txtbold\">.+?</span>")
     textBold = re.search(pattern, timeSection)
     if textBold == None:
-        time = '0'
+        time = 'now'
     else:
         textBold = textBold.group()
         if 'h' in textBold:
             time = textBold[22:len(textBold)-7]    #<span class="txtbold">11h27</span>
         else: 
             time = textBold[22:len(textBold)-7]    #<span class="txtbold">13</span>
+            time = setTimeToStandardFormat(time)
         
     return time
     
@@ -51,42 +61,55 @@ def getTimeNow(text):
         timeNow = re.search(r'\d\d?h\d\d', timeNowSection).group()
     return timeNow
 
-def getAll(content):
-    lineNeedToTrack = '230'
+def getAll(content, station):
     now = getTimeNow(content)
     if now:
-        print "Now is " + now + "\n"
-    else:
-        print "No tracking available"
-        return
-    
-    for busMatches in re.finditer(patternBus, content, re.IGNORECASE | re.DOTALL):    #for each line of bus
-        line = busMatches.group('line')
-        if line != lineNeedToTrack:
-            continue
-        print "Bus " + line + " is comming"
-        for timeMatches in re.finditer(patternTime, busMatches.group('timesheet'), re.IGNORECASE | re.DOTALL):    #for each line in timesheet
-            busTime = getTime(timeMatches.group('time'))
-            direction = timeMatches.group('direction')
-            if 'drale-Vieille Ville' not in direction:
-                continue
-            print (("\tat " if 'h' in busTime else "\tin ") + busTime + " min direction " + 
-                  direction +
-                  timeMatches.group('isRealtime').strip())
+        timeCheck = datetime.datetime.strptime(now, "%Hh%M")
+        #for each line of bus
+        for busMatches in re.finditer(patternBus, content, re.IGNORECASE | re.DOTALL):    
+            line = busMatches.group('line')
+            if line == lineNeedToTrack:
+                timeSheetNiceSophia[station] = list()
+                #for each line in timesheet
+                for timeMatches in re.finditer(patternTime, busMatches.group('timesheet'), re.IGNORECASE | re.DOTALL):    
+                    busTime = getTimeFrom(timeMatches.group('time')) + timeMatches.group('isRealtime').strip()
+                    direction = timeMatches.group('direction')
+                    if destinationToTrack in direction:
+                        timeSheetNiceSophia[station].append(busTime)
                   
 def getInfoByStation(station):
-    print "Checking ..."
     content = urllib.urlopen("https://cg06.tsi.cityway.fr/qrcode/" + str(station)).read()
-    #print content
-    getAll(content)
+    getAll(content, station)
     
+def show():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print ("Bus " + lineNeedToTrack + " direction " + destinationToTrack)
+    for stationNumber in timeSheetNiceSophia.keys():
+        print (mappingStationNiceSophia[stationNumber] , end='\t')
+        for time_t in timeSheetNiceSophia[stationNumber]:
+            print (time_t, end='\t')
+        print ('...')
+        
+def reorganizeBusTime():
+    nextBus = timeSheetNiceSophia[len(timeSheetNiceSophia)-1][0]
+    if 'h' in nextBus:
+        for stationNumber in reversed(timeSheetNiceSophia.keys()):
+            if (nextBus == 'passed' or timeSheetNiceSophia[stationNumber][0] > nextBus):
+                timeSheetNiceSophia[stationNumber].insert(0, 'passed')
+            nextBus = timeSheetNiceSophia[stationNumber][0]
+        
 def main():
-    stationNumber = raw_input("Station number: ")
     while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        getInfoByStation(stationNumber)
-        time.sleep(20)
-        #break
+        startTimer = time.time()
+        print ("Checking...")
+        timeSheetNiceSophia.clear()#clear list hours
+        for stationNumber in mappingStationNiceSophia.keys():
+            getInfoByStation(stationNumber)
+        show()
+        elapsedTimer = time.time() - startTimer
+        print ("Time execution: " + str(round(elapsedTimer,2)) + "s")
+        sleepTime = max(0,20 - elapsedTimer)
+        time.sleep(sleepTime)
         
 if __name__ == "__main__": 
     main()
